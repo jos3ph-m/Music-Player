6da9492203f2cb6acddcd6c89534da4e37692cf2
Joseph M <jmedina1985@hotmail.com>
2020-11-23 00:35:25 -0800

    fixed sass version

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,12 +1,17 @@
+//Import styles
+import "./styles/app.scss";
+
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
+import Bottom from './components/Bottom';
 
 function App() {
   return (
     <div className="App">
       <Song />
       <Player />
+      <Bottom />
     </div>
   );
 }
