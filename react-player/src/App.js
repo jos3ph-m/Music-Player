7c69bd311909d68aaa0563aa870d1b73ac0fd7da
Joseph M <jmedina1985@hotmail.com>
2020-11-23 00:42:26 -0800

    initial styles and resolved some formatting issues with _song and _player

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -4,14 +4,13 @@ import "./styles/app.scss";
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
-import Bottom from './components/Bottom';
+
 
 function App() {
   return (
     <div className="App">
       <Song />
       <Player />
-      <Bottom />
     </div>
   );
 }
