2db43f65b580796898462591d47b92c3cb5d80e6
Joseph M <jmedina1985@hotmail.com>
2020-11-24 23:40:08 -0800

    currentSong and setCurrentSong

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -8,12 +8,13 @@ import Player from './components/Player';
 import Song from './components/Song';
 
 // Import util
-import data from './util';
+import data from './data';
 
 
 function App() {
   //State
   const [ songs, setSongs ] = useState(data());
+  const [ currentSong, setCurrentSong ] = useState();
   return (
     <div className="App">
       <Song />
