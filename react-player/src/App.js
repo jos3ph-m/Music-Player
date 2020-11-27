1a7c22cf851f9e3e5e28ee41628d26a182ab2493
Joseph M <jmedina1985@hotmail.com>
2020-11-27 14:00:21 -0800

    Play and pause functionality

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -15,10 +15,11 @@ function App() {
   //State
   const [ songs, setSongs ] = useState(data());
   const [ currentSong, setCurrentSong ] = useState(songs[0]);
+  const [ isPlaying, setIsPlaying ] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} />
+      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
     </div>
   );
 }
--- react-player/src/components/Player.js
+++ react-player/src/components/Player.js
@@ -2,12 +2,18 @@ import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-const Player = ({ currentSong }) => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   // Ref
   const audioRef = useRef(null);
   // EventHandlers
   const playSongHandler = () => {
-    audioRef.current.play();
+    if(isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
   }
 
   return(
