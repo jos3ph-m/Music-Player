751b64801873ef3ae5cea6b258e5eddd373b527d
Joseph M <jmedina1985@hotmail.com>
2020-11-30 23:21:00 -0800

    added to app.js

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -6,6 +6,7 @@ import "./styles/app.scss";
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library'
 
 // Import util
 import data from './data';
@@ -20,6 +21,7 @@ function App() {
     <div className="App">
       <Song currentSong={currentSong} />
       <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong} />
+      <Library /> 
     </div>
   );
 }
