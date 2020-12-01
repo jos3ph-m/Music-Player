e1449c825683d565f35539e5681f71ae3adf3280
Joseph M <jmedina1985@hotmail.com>
2020-11-30 23:53:55 -0800

    Scroll styling

--- react-player/src/components/LibrarySong.js
+++ react-player/src/components/LibrarySong.js
@@ -5,8 +5,11 @@ const LibrarySong = ({ song }) => {
   return(
     <div className="library-song">
       <img src={song.cover} alt={song.name}></img>
-      <h3>{song.name}</h3>
-      <h4>{song.artist}</h4>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+      
     </div>
     
   )
