db9f18c6e66909e733918f4fb2c404c7d4a00ce2
Joseph M <jmedina1985@hotmail.com>
2020-12-01 23:27:29 -0800

    selectedSong setCurrentSong

--- react-player/src/components/Library.js
+++ react-player/src/components/Library.js
@@ -6,7 +6,7 @@ const Library = ({ songs, setCurrentSong }) => {
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
-        {songs.map(song => <LibrarySong setCurrentSong={setCurrentSong} song={song} />) }
+        {songs.map(song => <LibrarySong songs={songs} setCurrentSong={setCurrentSong} song={song} id={song.id} key={song.id} />) }
       </div>
     </div>
   )
--- react-player/src/components/LibrarySong.js
+++ react-player/src/components/LibrarySong.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id }) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === id)
+    // Be careful the filter above returns an array so we will pass in selectedSong[0] below
+    setCurrentSong(selectedSong[0]);
+  }
   return(
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
       <img src={song.cover} alt={song.name}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
