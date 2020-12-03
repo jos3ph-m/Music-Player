81251dc5698dfe8db263fb3fee7d1481316874a9
Joseph M <jmedina1985@hotmail.com>
2020-12-02 23:36:14 -0800

    Background on active style

--- react-player/src/components/LibrarySong.js
+++ react-player/src/components/LibrarySong.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 
-const LibrarySong = ({ song, songs, audioRef, setCurrentSong, id }) => {
+const LibrarySong = ({ song, songs, audioRef, setCurrentSong, id, isPlaying }) => {
   const songSelectHandler = () => {
     setCurrentSong(song)
-    audioRef.current.play();
+    // Check if the song is playing
+    if(isPlaying) {
+      const playPromise = audioRef.current.play()
+      if(playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play()
+        })
+      }
+    }
+    
   }
   return(
-    <div onClick={songSelectHandler} className="library-song">
+    <div onClick={songSelectHandler} className={`library-song ${song.active ? "selected" : ""}`}>
       <img src={song.cover} alt={song.name}></img>
       <div className="song-description">
         <h3>{song.name}</h3>
