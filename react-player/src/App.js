a41ff3cb271fc95d6b9c8e3135ce481273f7068d
Joseph M <jmedina1985@hotmail.com>
2020-12-03 14:18:37 -0800

    Add Nav component

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -7,6 +7,7 @@ import "./styles/app.scss";
 import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library'
+import Nav from './components/Nav'
 
 // Import util
 import data from './data';
